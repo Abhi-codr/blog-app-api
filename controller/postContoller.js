ce3ff19e97906a5666e0505af7b0acb6bbecfb29
Abhilash <abhilash.bharathi.bk@gmail.com>
2021-09-12 09:42:03 +0530

    fixed post capilized issue for heroku

--- controller/postContoller.js
+++ controller/postContoller.js
@@ -1,45 +1,55 @@
-const mongoose = require( "mongoose" );
-const Post = require( "../models/Post" );
-const { postInsertSchema, postUpdateSchema } = require( "../schemas/postSchema" );
+const mongoose = require("mongoose");
+const Post = require("../models/post");
+const { postInsertSchema, postUpdateSchema } = require("../schemas/postSchema");
 const catchAsync = require("../utils/catchAsync");
-const CustomError = require( "../utils/CustomError" );
+const CustomError = require("../utils/CustomError");
 
-const insertPost = catchAsync(async(req,res,next)=>{
+const insertPost = catchAsync(async (req, res, next) => {
   const { error } = postInsertSchema.validate(req.body);
   if (error) {
     return next(new CustomError(400, error.details[0].message));
   }
-    const {title,content,createdBy} = req.body;
-    const post = Post({title,content,createdBy});
-    await post.save()
-    res.status(201).json({status:"success",data:{_id:post._id}})
-})
+  const { title, content, createdBy } = req.body;
+  const post = Post({ title, content, createdBy });
+  await post.save();
+  res.status(201).json({ status: "success", data: { _id: post._id } });
+});
 
-const getAllPosts = catchAsync(async(req,res,next)=>{
-    const posts =await Post.find().select(["-__v"]).populate("createdBy","name",)
-    res.status(201).json({status:"success",data:posts})
-})
+const getAllPosts = catchAsync(async (req, res, next) => {
+  const posts = await Post.find()
+    .select(["-__v"])
+    .populate("createdBy", "name");
+  res.status(201).json({ status: "success", data: posts });
+});
 
-const getPost = catchAsync(async(req,res,next)=>{
-    if(!mongoose.Types.ObjectId.isValid(req.params.id)){return next(new CustomError(400,"Invalid Id"))}
-    const posts =await Post.findById(req.params.id).select(["-__v"]).populate("createdBy","name",)
-    res.status(201).json({status:"success",data:posts})
-})
+const getPost = catchAsync(async (req, res, next) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return next(new CustomError(400, "Invalid Id"));
+  }
+  const posts = await Post.findById(req.params.id)
+    .select(["-__v"])
+    .populate("createdBy", "name");
+  res.status(201).json({ status: "success", data: posts });
+});
 
-const updatePost = catchAsync(async(req,res,next)=>{
-    const { error } = postUpdateSchema.validate(req.body);
+const updatePost = catchAsync(async (req, res, next) => {
+  const { error } = postUpdateSchema.validate(req.body);
   if (error) {
     return next(new CustomError(400, error.details[0].message));
   }
-    if(!mongoose.Types.ObjectId.isValid(req.params.id)){return next(new CustomError(400,"Invalid Id"))}
-    const post =await Post.updateOne({_id:req.params.id},req.body)
-    res.status(201).json({status:"success",data:post})
-})
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return next(new CustomError(400, "Invalid Id"));
+  }
+  const post = await Post.updateOne({ _id: req.params.id }, req.body);
+  res.status(201).json({ status: "success", data: post });
+});
 
-const deletePost = catchAsync(async(req,res,next)=>{
-    if(!mongoose.Types.ObjectId.isValid(req.params.id)){return next(new CustomError(400,"Invalid Id"))}
-    const post =await Post.deleteOne({_id:req.params.id})
-    res.status(201).json({status:"success",data:post})
-})
+const deletePost = catchAsync(async (req, res, next) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return next(new CustomError(400, "Invalid Id"));
+  }
+  const post = await Post.deleteOne({ _id: req.params.id });
+  res.status(201).json({ status: "success", data: post });
+});
 
-module.exports = {insertPost,getAllPosts,deletePost,updatePost,getPost}
+module.exports = { insertPost, getAllPosts, deletePost, updatePost, getPost };
